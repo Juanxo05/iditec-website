e5b9e1842b80c787773e778e7e4f5d2c40d45296
Marcos Felipe Cárdenas Hinostroza <marcosfelipe.cardenash@gmail.com>
2017-06-16 00:02:10 -0400

    cambios contactos
    Contactos funcionando (index 2)

---
+++ contact-form.php
@@ -0,0 +1,42 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+require_once 'phpmailer/PHPMailerAutoload.php';
+
+if (isset($_POST['inputName']) && isset($_POST['inputEmail']) && isset($_POST['inputSubject']) && isset($_POST['inputMessage'])) {
+
+    //check if any of the inputs are empty
+    if (empty($_POST['inputName']) || empty($_POST['inputEmail']) || empty($_POST['inputSubject']) || empty($_POST['inputMessage'])) {
+        $data = array('success' => false, 'message' => 'Please fill out the form completely.');
+        echo json_encode($data);
+        exit;
+    }
+
+    //create an instance of PHPMailer
+    $mail = new PHPMailer();
+
+    $mail->From = $_POST['inputEmail'];
+    $mail->FromName = $_POST['inputName'];
+    $mail->AddAddress('by.yo.69@gmail.com'); //recipient 
+    $mail->Subject = $_POST['inputSubject'];
+    $mail->Body = "Nombre: " . $_POST['inputName'] . "\r\n\r\nMensaje: " . stripslashes($_POST['inputMessage']);
+
+    if (isset($_POST['ref'])) {
+        $mail->Body .= "\r\n\r\nRef: " . $_POST['ref'];
+    }
+
+    if(!$mail->send()) {
+        $data = array('success' => false, 'message' => 'Mensaje no enviado. Error Mailer: ' . $mail->ErrorInfo);
+        echo json_encode($data);
+        exit;
+    }
+
+    $data = array('success' => true, 'message' => 'Gracias! Hemos recibido su mensaje.');
+    echo json_encode($data);
+
+} else {
+
+    $data = array('success' => false, 'message' => 'Please fill out the form completely.');
+    echo json_encode($data);
+
+}
